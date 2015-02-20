b38789430a3a07bfd019a3e5cda2fc1743985f66
BogdanRusinka <rusinka.bogdan@gmail.com>
2015-02-20 18:24:46 +0200

    Add new styles for page

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 angular.module('binaryApp',[])
 	.controller('MainController', ['$http', '$scope', function($http, $scope){
+		$scope.publicTotal = 0;
 		$http.get('https://api.github.com/users/B1naryStudio/repos').success(function(data, status, headers, config){
 			$scope.repositories = data;
+			data.forEach(function(item){
+				if (!item.private){
+					$scope.publicTotal++;
+				}
+			});
 		});
 
 		$scope.icon = {
 			'JavaScript' : 'js.png',
 			'PHP' : 'php.png',
-			'C#' : 'csharp.png'
+			'C#' : 'csharp.png',
+			'HTML' : 'html.png'
 		};
+
+		 $scope.filter = {};
+	    
+	    $scope.filterByCategory = function (repository) {
+	        return $scope.filter[repository.language] || noFilter($scope.filter);
+	    };
+	    
+	    function noFilter(filterObj) {
+	        for (var key in filterObj) {
+	            if (filterObj[key]) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
 
 	}]);
